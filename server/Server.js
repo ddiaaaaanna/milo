0087db5e95132dc4a0d9a0fc3db7bf03b728478e
ddiaaaaanna <dpuraite@yahoo.com>
2026-06-18 09:39:25 +0300

    connect mongoDB and verify database connection

--- server/Server.js
+++ server/Server.js
@@ -1,12 +1,25 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
 const express = require("express");
 
 const app = express();
-const port = 5001;
+const port = process.env.PORT || 5001;
+
+async function startApp() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log("Server running on port 5001");
+    });
+
+    console.log("mongoDB connected");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+startApp();
 
 app.get("/", (req, res) => {
   res.send("Milo API Running");
-});
-
-app.listen(port, () => {
-  console.log("Server running on port 5001");
 });
