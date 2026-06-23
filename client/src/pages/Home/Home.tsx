dcadd187fe0a83c11ec45e140ba2fec1ef6ccb59
ddiaaaaanna <dpuraite@yahoo.com>
2026-06-23 13:17:15 +0300

    add home page with dog list linking to profiles

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,9 +1,22 @@
 import "./Home.css";
+import useFetch from "../../hooks/useFetch";
+import { type Dog } from "../../types/dog";
+import { Link } from "react-router-dom";
 
 function Home() {
+  const api = `http://localhost:5001/dogs`;
+  const { data } = useFetch<Dog[]>(api);
+  if (!data) return <p>Loading…</p>;
+
   return (
     <>
       <h1>homepage</h1>
+      {data &&
+        data.map((d) => (
+          <Link to={`/dog/${d._id}`} key={d._id}>
+            <p>{d.name}</p>
+          </Link>
+        ))}
     </>
   );
 }
