c8e83a66c09308b36c435fcd5f975597d1228800
ddiaaaaanna <dpuraite@yahoo.com>
2026-07-12 17:44:46 +0300

    add shared styling to edit exercise component

--- client/src/components/Training/ExerciseEditor.tsx
+++ client/src/components/Training/ExerciseEditor.tsx
@@ -1,4 +1,5 @@
-import "./ExerciseEditor.css";
+import { NotebookPen } from "lucide-react";
+import "../../styles/FormStyles.css";
 import type { TrainingType } from "../../types/training";
 import { useState, type SyntheticEvent } from "react";
 
@@ -66,70 +67,97 @@ function ExerciseEditor({
   }
 
   return (
-    <>
-      {savedEntry && (
-        <>
-          <p>Your exercise was edited</p>
-          <button onClick={resetForms}>See Training</button>
-        </>
-      )}
-
-      {!savedEntry && (
-        <form onSubmit={updateTraining}>
-          <p>{editedTraining.difficulty}</p>
-          <p>{editedTraining.exerciseName}</p>
-          <button type="button" onClick={resetForms}>
-            x
-          </button>
-
-          <p>Status</p>
-
-          <div>
-            <button
-              type="button"
-              onClick={() =>
-                setEditedTraining({ ...editedTraining, status: "not-started" })
-              }
-            >
-              Not Started
-            </button>
-            <button
-              type="button"
-              onClick={() =>
-                setEditedTraining({ ...editedTraining, status: "in-progress" })
-              }
-            >
-              In Progress
-            </button>
-            <button
-              type="button"
-              onClick={() =>
-                setEditedTraining({ ...editedTraining, status: "mastered" })
-              }
-            >
-              Mastered
-            </button>
+    <div className="form-container">
+      <div className="form-modal">
+        {savedEntry && (
+          <div className="form-saved">
+            <p>Your exercise was edited</p>
+            <button onClick={resetForms}>See Training</button>
           </div>
+        )}
 
-          <label htmlFor="edit-exercise-description">Description / goal </label>
-          <textarea
-            id="edit-exercise-description"
-            value={editedTraining.description}
-            onChange={(e) =>
-              setEditedTraining({
-                ...editedTraining,
-                description: e.target.value,
-              })
-            }
-          ></textarea>
+        {!savedEntry && (
+          <>
+            <div className="form-header">
+              <div className="form-header-info">
+                <NotebookPen />
+                <div className="form-header-txt">
+                  <p>{editedTraining.difficulty}</p>
+                  <p>{editedTraining.exerciseName}</p>
+                </div>
+              </div>
 
-          <button type="button" onClick={resetForms}>
-            Cancel
-          </button>
-          <button type="submit">Save changes</button>
-        </form>
-      )}
-    </>
+              <button type="button" onClick={resetForms}>
+                x
+              </button>
+            </div>
+
+            <form onSubmit={updateTraining} className="form-content">
+              <div className="form-scroll">
+                <p>Status</p>
+
+                <div>
+                  <button
+                    type="button"
+                    onClick={() =>
+                      setEditedTraining({
+                        ...editedTraining,
+                        status: "not-started",
+                      })
+                    }
+                  >
+                    Not Started
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() =>
+                      setEditedTraining({
+                        ...editedTraining,
+                        status: "in-progress",
+                      })
+                    }
+                  >
+                    In Progress
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() =>
+                      setEditedTraining({
+                        ...editedTraining,
+                        status: "mastered",
+                      })
+                    }
+                  >
+                    Mastered
+                  </button>
+                </div>
+
+                <label htmlFor="edit-exercise-description">
+                  Description / goal{" "}
+                </label>
+                <textarea
+                  id="edit-exercise-description"
+                  value={editedTraining.description}
+                  onChange={(e) =>
+                    setEditedTraining({
+                      ...editedTraining,
+                      description: e.target.value,
+                    })
+                  }
+                ></textarea>
+              </div>
+
+              <div className="button-container">
+                <button type="button" onClick={resetForms}>
+                  Cancel
+                </button>
+                <button type="submit">Save changes</button>
+              </div>
+            </form>
+          </>
+        )}
+      </div>
+    </div>
   );
 }
 
